180eeaaa26ce32fe648b137bee4dfde050fe8e16
maaz-shahid99 <goluthakle@gmail.com>
2024-04-17 15:33:25 +0530

    Cursor MOvement Optimised

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import time
 import numpy as np
 import mediapipe as mp
 import pyautogui
+import win32api
 
 # Define the dimensions of the camera input and the region of interest (ROI)
 wCam, hCam = 640, 480
@@ -43,81 +44,88 @@ while True:
     _, img = cap.read()
 
     #BGR to RGB conversion
-    RGB_frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # RGB_frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     
     #Detections
-    result = detector.process(RGB_frame)
+    result = detector.process(img)
 
+    RGB_frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    hand_landmarks = None  # Initialize hand_landmarks outside the loop
+    
     if result.multi_hand_landmarks:
-        for hand_landmarks in result.multi_hand_landmarks:
-            # print(hand_landmarks)
-            mp_drawing.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS, 
-                                      mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2),
-                                      mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=2))
-            landmarks = hand_landmarks
+        # Access the first hand_landmarks directly without looping
+        hand_landmarks = result.multi_hand_landmarks[0]
+        mp_drawing.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS, 
+                                  mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2),
+                                  mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=2)
+                                  )
 
-        #2. Get the tip of Index and Middle Fingers        
-        
-            for id, landmark in enumerate(hand_landmarks.landmark): 
+    #2. Get the tip of Index and Middle Fingers        
+    
+    if hand_landmarks:
+        for id, landmark in enumerate(hand_landmarks.landmark): 
+            # print(landmark.x, landmark.y)
+            x = int(landmark.x *wCam)
+            y = int(landmark.y *hCam)
+            # print(x,y)
+
+            if id == 3: #Thumb Knuckle
+                cv2.circle(img=img, center=(x,y), radius=20, color=(0,255,255))
                 
-                x = int(landmark.x *wCam)
-                y = int(landmark.y *hCam)
-                # print(x,y)
-                
-                if id == 3: #Thumb Knuckle
-                    cv2.circle(img=img, center=(x,y), radius=20, color=(0,255,255))
+                knuckle_x = screen_width - (x * screen_width / wCam)
+                knuckle_y = screen_height/hCam*y
+
+                if 0 < knuckle_x < screen_width and 0 < knuckle_y < screen_height:
+                    if prev_cursor_x is None or prev_cursor_y is None:
+                        smoothed_x = knuckle_x
+                        smoothed_y = knuckle_y
+                    else:
+                        smoothed_x = alpha * knuckle_x + (1 - alpha) * prev_cursor_x
+                        smoothed_y = alpha * knuckle_y + (1 - alpha) * prev_cursor_y
+
+                    # Move cursor to the smoothed position
                     
-                    knuckle_x = (screen_width - (x * screen_width / wCam))
-                    knuckle_y = screen_height/hCam*y
+                    win32api.SetCursorPos((int(smoothed_x),int(smoothed_y)))
+                    print(int(smoothed_x),int(smoothed_y),x,y)
 
-                    if 0 < knuckle_x < screen_width and 0 < knuckle_y < screen_height:
-                        if prev_cursor_x is None or prev_cursor_y is None:
-                            smoothed_x = knuckle_x
-                            smoothed_y = knuckle_y
-                        else:
-                            smoothed_x = alpha * knuckle_x + (1 - alpha) * prev_cursor_x
-                            smoothed_y = alpha * knuckle_y + (1 - alpha) * prev_cursor_y
+                    # Update previous cursor position
+                    prev_cursor_x = smoothed_x
+                    prev_cursor_y = smoothed_y
 
-                        # Move cursor to the smoothed position
-                        pyautogui.moveTo(smoothed_x, smoothed_y)
+                    if abs(knuckle_y - index_y) < 50 and not left_click_flag:
+                        print('left click')
+                        pyautogui.click()
+                        left_click_flag = True
+                        right_click_flag = False
 
-                        # Update previous cursor position
-                        prev_cursor_x = smoothed_x
-                        prev_cursor_y = smoothed_y
-
-                        if abs(knuckle_y - index_y) < 50 and not left_click_flag:
-                            print('left click')
-                            pyautogui.click()
-                            left_click_flag = True
-                            right_click_flag = False
-
-                        elif abs(knuckle_y - mid_fing_y) < 50 and not right_click_flag:
-                            print('right click')
-                            pyautogui.click(button='right')
-                            right_click_flag = True
-                            left_click_flag = False
-                    
-                        else:
-                            left_click_flag = False
-                            right_click_flag = False
-
-                if id == 8: #Index Finger
-                    cv2.circle(img=img, center=(x,y), radius=20, color=(0,255,255))
-                    index_x = (screen_width - (x * screen_width / wCam))
-                    index_y = (screen_height/hCam*y)
-                    
-
-                if id == 12: #Middle Finger
-                    cv2.circle(img=img, center=(x,y), radius=20, color=(0,255,255))
-                    mid_fing_x = screen_width - (x * screen_width / wCam)
-                    mid_fing_y = screen_height/hCam*y
-                    
-                    # Add a threshold for double-click detection
-                    if abs(index_x - mid_fing_x) < 30 and not right_click_flag and not left_click_flag:
-                        print('double click')
-                        pyautogui.doubleClick()
+                    elif abs(knuckle_y - mid_fing_y) < 50 and not right_click_flag:
+                        print('right click')
+                        pyautogui.click(button='right')
                         right_click_flag = True
                         left_click_flag = False
+                
+                    else:
+                        left_click_flag = False
+                        right_click_flag = False
+
+            if id == 8: #Index Finger
+                cv2.circle(img=img, center=(x,y), radius=20, color=(0,255,255))
+                index_x = (screen_width - (x * screen_width / wCam))
+                index_y = (screen_height/hCam*y)
+                
+
+            if id == 12: #Middle Finger
+                cv2.circle(img=img, center=(x,y), radius=20, color=(0,255,255))
+                mid_fing_x = screen_width - (x * screen_width / wCam)
+                mid_fing_y = screen_height/hCam*y
+                
+                # Add a threshold for double-click detection
+                if abs(index_x - mid_fing_x) < 30 and not right_click_flag and not left_click_flag:
+                    print('double click')
+                    pyautogui.doubleClick()
+                    right_click_flag = True
+                    left_click_flag = False
 
     #11. Frame Rate
     cTime = time.time()
