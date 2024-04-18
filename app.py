9d66b6c7e91793fbc5280622e421fe847e0421ea
unknown <32sinjinihomroy@gmail.com>
2024-04-18 16:05:17 +0530

    no changes

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ wCam, hCam = 640, 480
 screen_width, screen_height = pyautogui.size()
 pTime = 0
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
 
@@ -18,7 +18,7 @@ cap.set(4, hCam)
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 detector = mp_hands.Hands(max_num_hands=1,
-                      min_detection_confidence=0.7, 
+                      min_detection_confidence=0.7,
                       min_tracking_confidence=0.5)
 
 index_y = 0
@@ -48,25 +48,25 @@ prev_cursor_y = None
 def map_value_x(value):
     input_min, input_max = input_range_x
     output_min, output_max = (0,640)
-    
+
     # Map the input value to the output range
     mapped_value = output_min + (output_max - output_min) * ((value - input_min) / (input_max - input_min))
-    
+
     # Make sure the mapped value stays within the output range
     mapped_value = max(min(mapped_value, output_max), output_min)
-    
+
     return int(mapped_value)
 
 def map_value_y(value):
     input_min, input_max = input_range_y
     output_min, output_max = (0,480)
-    
+
     # Map the input value to the output range
     mapped_value = output_min + (output_max - output_min) * ((value - input_min) / (input_max - input_min))
-    
+
     # Make sure the mapped value stays within the output range
     mapped_value = max(min(mapped_value, output_max), output_min)
-    
+
     return int(mapped_value)
 
 
@@ -78,26 +78,26 @@ while True:
 
     #BGR to RGB conversion
     # RGB_frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    
+
     #Detections
     result = detector.process(img)
 
     RGB_frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     hand_landmarks = None  # Initialize hand_landmarks outside the loop
-    
+
     if result.multi_hand_landmarks:
         # Access the first hand_landmarks directly without looping
         hand_landmarks = result.multi_hand_landmarks[0]
-        mp_drawing.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS, 
+        mp_drawing.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS,
                                   mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2),
                                   mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=2)
                                   )
 
-    #2. Get the tip of Index and Middle Fingers        
-    
+    #2. Get the tip of Index and Middle Fingers
+
     if hand_landmarks:
-        for id, landmark in enumerate(hand_landmarks.landmark): 
+        for id, landmark in enumerate(hand_landmarks.landmark):
             # print(landmark.x, landmark.y)
             x = int(landmark.x *wCam)
             y = int(landmark.y *hCam)
@@ -108,7 +108,7 @@ while True:
 
                 x = map_value_x(x)
                 y = map_value_y(y)
-                
+
                 knuckle_x = screen_width - (x * screen_width / wCam)
                 knuckle_y = screen_height/hCam*y
 
@@ -121,7 +121,7 @@ while True:
                         smoothed_y = alpha * knuckle_y + (1 - alpha) * prev_cursor_y
 
                     # Move cursor to the smoothed position
-                    
+
                     win32api.SetCursorPos((int(smoothed_x),int(smoothed_y)))
                     # print(int(smoothed_x),int(smoothed_y),x,y)
 
@@ -140,7 +140,7 @@ while True:
                         pyautogui.click(button='right')
                         right_click_flag = True
                         left_click_flag = False
-                
+
                     else:
                         left_click_flag = False
                         right_click_flag = False
@@ -149,13 +149,13 @@ while True:
                 cv2.circle(img=img, center=(x,y), radius=20, color=(0,255,255))
                 index_x = (screen_width - (x * screen_width / wCam))
                 index_y = (screen_height/hCam*y)
-                
+
 
             if id == 12: #Middle Finger
                 cv2.circle(img=img, center=(x,y), radius=20, color=(0,255,255))
                 mid_fing_x = screen_width - (x * screen_width / wCam)
                 mid_fing_y = screen_height/hCam*y
-                
+
                 # Add a threshold for double-click detection
                 if abs(index_x - mid_fing_x) < 20 and not right_click_flag and not left_click_flag:
                     print('double click')
@@ -174,11 +174,8 @@ while True:
 
     #12. Display
 
-    
+
     cv2.imshow("Imshow", img)
 
     if cv2.waitKey(10) == ord('q'):
         break
-
-
-
